b3eda73d984a8bdd7419c543c19961bf4eba981e
sanbedoyau <sanbedoya@unal.edu.co>
2025-05-12 17:28:04 -0500

    Lab Done

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,20 +1,49 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta.
-"""
-
-# pylint: disable=import-outside-toplevel
-
+import pandas as pd
 
 def pregunta_01():
-    """
-    Construya y retorne un dataframe de Pandas a partir del archivo
-    'files/input/clusters_report.txt'. Los requierimientos son los siguientes:
+  with open('files/input/clusters_report.txt') as file:
+    lines = file.read()
 
-    - El dataframe tiene la misma estructura que el archivo original.
-    - Los nombres de las columnas deben ser en minusculas, reemplazando los
-      espacios por guiones bajos.
-    - Las palabras clave deben estar separadas por coma y con un solo
-      espacio entre palabra y palabra.
+  lines = lines.replace('.', '')
+  lines = lines.split('\n')
 
+  columns1 = lines[0].strip().replace(' ', '_')
+  columns2 = lines[1].ljust(len(columns1), '_').replace(' ', '_')
 
-    """
+  mayusculas = []
+  for letter in columns1:
+    if letter.isupper():
+      mayusculas.append(letter)
+  for letter in list(set(mayusculas)):
+    columns1 = columns1.replace(letter, f'¿{letter}')
+  columns1 = columns1.split('¿')
+  col = []
+  for word in columns1:
+    if word == '':
+      continue
+    col.append(word)
+  start = 0
+  columns = []
+  for i in range(len(col)):
+    end = start + len(col[i])
+    columns.append(col[i] + columns2[start:end])
+    start = end
+  columns = list(map(lambda x: '_'.join([parte for parte in x.strip('_').lower().split('_') if parte != '']), columns))
+
+  data = {key: [] for key in columns}
+  cont = -1
+  for line in lines[4:]:
+    split_line = line.split()
+    if len(line.split()) == 0:
+      continue
+    if split_line[0].isdigit():
+      cont += 1
+      data[columns[0]].append(int(split_line[0]))
+      data[columns[1]].append(int(split_line[1]))
+      data[columns[2]].append(float(split_line[2].replace(',','.')))
+      data[columns[3]].append('')
+      clave = ' '.join(split_line[4:])
+    else:
+      clave = ' ' + ' '.join(split_line)
+    data[columns[3]][cont] += clave
+  return pd.DataFrame(data = data)
